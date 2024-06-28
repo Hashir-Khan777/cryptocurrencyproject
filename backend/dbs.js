28509398c6d01c702894acfff942787c8595d5b7
Hashir Khan <khnhashir555@gmail.com>
2024-06-28 22:37:52 +0500

    changes

--- backend/dbs.js
+++ backend/dbs.js
@@ -4,17 +4,20 @@ module.exports = {
     password: "AVNS_B8fk_9sK0YA7oE2r1U2",
     database: "inventonbtc",
     host: "pg-f0e7207-hashirk966-d8b9.f.aivencloud.com",
+    port: 12822,
   },
   TEST: {
     username: "avnadmin",
     password: "AVNS_B8fk_9sK0YA7oE2r1U2",
     database: "inventonbtc",
     host: "pg-f0e7207-hashirk966-d8b9.f.aivencloud.com",
+    port: 12822,
   },
   PRODUCTION: {
     username: "avnadmin",
     password: "AVNS_B8fk_9sK0YA7oE2r1U2",
     database: "inventonbtc",
     host: "pg-f0e7207-hashirk966-d8b9.f.aivencloud.com",
+    port: 12822,
   },
 };
